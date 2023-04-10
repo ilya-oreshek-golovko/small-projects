18c0a328fb815d8c5a7de0140fdbd73b95d024f9
Головко Илья Владимирович <igolovko@bostonsd.ru>
2023-04-10 23:29:53 +0300

    completed goblin website

--- Beautifull Pages/first/first.js
+++ Beautifull Pages/first/first.js
@@ -4,3 +4,10 @@ const menu = document.querySelector('.menu');
 btnToggle.addEventListener("click", (evt) => {
 	menu.classList.toggle('is-open');
 });
+
+const handledCustomersInfo = new Map();
+handledCustomersInfo.set(111, {qq: 11, ww: 1111});
+handledCustomersInfo.set(222, {qq: 22, ww: 2222});
+for(const [key, value] of handledCustomersInfo.entries()){
+	console.log(key + " : " + value.qq + ", " + value.ww );
+}
